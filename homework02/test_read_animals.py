0ac7816eb0fb34d5bf2497c50912ca9782175f31
jweaston <jweaston@isp02.tacc.utexas.edu>
2021-02-24 15:18:01 -0600

    updated test script

--- homework02/test_read_animals.py
+++ homework02/test_read_animals.py
@@ -4,15 +4,18 @@ from read_animals import breed
 
 class TestReadAnimals(unittest.TestCase):
     def test_breed(self):
-        self.assertEqual(breed({'head': 'bull', 'body': 'salmon-mantis', 'arms': 2, 'legs': 9, 'tails': 11} ,
-                               {'head': 'snake', 'body': 'beagle-oriole', 'arms': 10, 'legs': 9, 'tails': 19} ),
-                               {'head': 'bull-snake', 'body': 'salmon-mantis-beagle-oriole', 'arms': 6, 'legs': 9, 'tails': 15})
+        animal1 = {'head': 'bull', 'body': 'salmon-mantis', 'arms': 2, 'legs': 9, 'tails': 11} 
+        animal2 = {'head': 'snake', 'body': 'beagle-oriole', 'arms': 10, 'legs': 9, 'tails': 19}
+        child = {'head': 'bull-snake', 'body': 'salmon-mantis-beagle-oriole', 'arms': 6, 'legs': 9, 'tails': 15}
+        self.assertEqual(breed(animal1, animal2),child)
         self.assertRaises(KeyError, breed,{'head':'raven','body':'lion-tiger','arms':5,'legs':6,'tail':11},
                                           {'heads':'lion'})
         self.assertRaises(KeyError, breed,{'head':'raven','body':'lion-tiger','arms':5,'legs':6,'tail':11},
                                           {'head':'lion'})
         self.assertRaises(AssertionError, breed,'lion','goat')
         self.assertRaises(AssertionError, breed,{'head':'bunny','body':'flamingo-dog','arms':3,'legs':6,'tail':9},'zebra')
+        self.assertRaises(AssertionError, breed, 'str', 'str')
+
 
 if __name__ == '__main__':
     unittest.main()
