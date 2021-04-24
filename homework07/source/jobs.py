9759ddec99e39124967184ff2a95783da08e5ee9
jweaston <jweaston@isp02.tacc.utexas.edu>
2021-04-24 17:37:59 -0500

    Updated python source code

--- homework07/source/jobs.py
+++ homework07/source/jobs.py
@@ -1,10 +1,15 @@
 # jobs.py
 import uuid 
+import os
 from hotqueue import HotQueue
 from redis import StrictRedis
 
-q = HotQueue("queue", host='172.17.0.1', port=6379, db=1)
-rd = StrictRedis(host='172.17.0.1', port=6379, db=0)
+redis_ip = os.environ.get('REDIS_IP')
+if not redis_ip:
+    raise Exception()
+
+q = HotQueue("queue", host=redis_ip, port=6379, db=1)
+rd = StrictRedis(host=redis_ip, port=6379, db=0)
 
 def _generate_jid():
     return str(uuid.uuid4())
@@ -43,12 +48,12 @@ def add_job(start, end, status="submitted"):
     _queue_job(jid)
     return job_dict
 
-def update_job_status(jid, status):
+def update_job_status(jid, new_status):
     """Update the status of job with job id `jid` to status `status`."""
     jid, status, start, end = rd.hmget(_generate_job_key(jid), 'id', 'status', 'start', 'end')
     job = _instantiate_job(jid, status, start, end)
     if job:
-        job['status'] = status
-        _save_job(_generate_job_key(jid), job)
+        job['status'] = new_status
+        _save_job(_generate_job_key(job['id']), job)
     else:
         raise Exception()
--- homework07/source/worker.py
+++ homework07/source/worker.py
@@ -2,9 +2,15 @@
 import time
 from jobs import q, update_job_status
 
-@q.worker
-def execute_job(jid):
+
+for jid in q.consume():
     update_job_status(jid, 'in progress')
     # todo - replace with real job
     time.sleep(15)
     update_job_status(jid, 'complete')
+# @q.worker
+# def execute_job(jid):
+#     update_job_status(jid, 'in progress')
+#     # todo - replace with real job
+#     time.sleep(15)
+#     update_job_status(jid, 'complete')
